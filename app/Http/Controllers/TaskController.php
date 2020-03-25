10dcbc871fc75d70394260ea77a2d397e0a38d87
ishakdeveloper <55481431+ishakdeveloper@users.noreply.github.com>
2020-03-25 15:04:57 +0100

    new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   .styleci.yml 	new file:   README.md 	new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/FileUpload.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Controllers/TaskController.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/Authenticate.php 	new file:   app/Http/Middleware/CheckForMaintenanceMode.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/TrimStrings.php 	new file:   app/Http/Middleware/TrustProxies.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   app/Todo.php 	new file:   app/User.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/cors.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/hashing.php 	new file:   config/logging.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/TodoFactory.php 	new file:   database/factories/UserFactory.php 	new file:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2019_08_19_000000_create_failed_jobs_table.php 	new file:   database/migrations/2020_03_22_181105_create_todos_table.php 	new file:   database/seeds/DatabaseSeeder.php 	new file:   database/seeds/TodoSeeder.php 	new file:   package-lock.json 	new file:   package.json 	new file:   phpunit.xml 	new file:   public/.htaccess 	new file:   public/favicon.ico 	new file:   public/index.php 	new file:   public/robots.txt 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   resources/lang/en/auth.php 	new file:   resources/lang/en/pagination.php 	new file:   resources/lang/en/passwords.php 	new file:   resources/lang/en/validation.php 	new file:   resources/sass/app.scss 	new file:   resources/views/welcome.blade.php 	new file:   routes/api.php 	new file:   routes/channels.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   server.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/cache/data/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/testing/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/CreatesApplication.php 	new file:   tests/Feature/ExampleTest.php 	new file:   tests/TestCase.php 	new file:   tests/Unit/ExampleTest.php 	new file:   webpack.mix.js

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Todo::all(); // Pak alle todos
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        $todo = Todo::create($data);
+
+        return response($todo, 201);
+ 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        $data = $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        $todo->update($data);
+
+        return response($todo, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response('Todo verwijderd', 200);
+    }
+}
